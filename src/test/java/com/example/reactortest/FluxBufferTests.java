e062addf4a13b9e41d60128b3e30a575c710c576
dckat <kdc9619@naver.com>
2023-04-07 19:28:10 +0900

    test: 리액터 버퍼링 테스트 클래스 작성

---
+++ src/test/java/com/example/reactortest/FluxBufferTests.java
@@ -0,0 +1,75 @@
+package com.example.reactortest;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class FluxBufferTests {
+
+    // 일정 갯수 단위로 Flux 버퍼링
+    @Test
+    public void buffer() {
+        Flux<String> fruitFlux = Flux.just(
+                "apple", "orange", "banana", "kiwi", "strawberry");
+
+        Flux<List<String>> bufferedFlux = fruitFlux.buffer(3);
+
+        StepVerifier.create(bufferedFlux)
+                .expectNext(Arrays.asList("apple", "orange", "banana"))
+                .expectNext(Arrays.asList("kiwi", "strawberry"))
+                .verifyComplete();
+    }
+
+    // flatMap 활용 Flux 버퍼링
+    @Test
+    public void bufferAndflatMap() {
+        Flux.just("apple", "orange", "banana", "kiwi", "strawberry")
+                .buffer(3)
+                .flatMap(x ->
+                        Flux.fromIterable(x)
+                                .map(y -> y.toUpperCase())
+                                .subscribeOn(Schedulers.parallel())
+                                .log()
+                ).subscribe();
+    }
+
+    // 모든 항목을 List로 버퍼링
+    @Test
+    public void collectList() {
+        Flux<String> fruitFlux = Flux.just(
+                "apple", "orange", "banana", "kiwi", "strawberry");
+
+        Mono<List<String>> fruitListMono = fruitFlux.collectList();
+
+        StepVerifier.create(fruitListMono)
+                .expectNext(Arrays.asList(
+                        "apple", "orange", "banana", "kiwi", "strawberry"))
+                .verifyComplete();
+    }
+
+    // 모든 항목을 Map으로 버퍼링
+    @Test
+    public void collectMap() {
+        Flux<String> animalFlux = Flux.just(
+                "aardvark", "elephant", "koala", "eagle", "kangaroo");
+
+        Mono<Map<Character, String>> animalMapMono =
+                animalFlux.collectMap(a -> a.charAt(0));
+
+        StepVerifier.create(animalMapMono)
+                .expectNextMatches(map -> {
+                    return
+                            map.size() == 3 &&
+                                    map.get('a').equals("aardvark") &&
+                                    map.get('e').equals("eagle") &&
+                                    map.get('k').equals("kangaroo");
+                })
+                .verifyComplete();
+    }
+}
